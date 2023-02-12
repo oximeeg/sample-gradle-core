6d14e441324cfa0eb401aafd57382a8359b09e1e
oximeeg <oximeeg@gmail.com>
2023-02-12 16:49:17 +0900

    v2.0.0

--- lib/src/main/java/com/oximeeg/sample/core/Core.java
+++ lib/src/main/java/com/oximeeg/sample/core/Core.java
@@ -1,7 +1,7 @@
 package com.oximeeg.sample.core;
 
 public class Core {
-    public String echo(String arg1) {
-        return "CoreMethod arg1:" + arg1;
+    public String echo(String arg1, String arg2) {
+        return "CoreMethod arg1:" + arg1 + " arg2:" + arg2;
     }
 }
